494f07ab8cb89ccfab080dcdc49def3cd7462e80
MatheusEstanislau <matheus.estanislau7@gmail.com>
2020-11-23 15:42:01 -0300

    adiconado logica de adicionar ao carrinho no reducer cart

--- modulo07/src/store/modules/cart/reducer.js
+++ modulo07/src/store/modules/cart/reducer.js
@@ -1,5 +1,11 @@
-const cart = () => {
-  return [];
+/* eslint-disable no-unused-vars */
+const cart = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      return [...state, action.product];
+    default:
+      return state;
+  }
 };
 
 export default cart;
