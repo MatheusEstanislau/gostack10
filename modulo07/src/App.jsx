c5a6109a0802c468e217c64e693a2e359ad17ffa
MatheusEstanislau <matheus.estanislau7@gmail.com>
2020-11-23 12:41:34 -0300

    adicionado redux e react redux

--- modulo07/src/App.jsx
+++ modulo07/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import GlobalStyle from './styles/global';
+import store from './store';
 
 import Router from './routes';
 
@@ -8,11 +10,13 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Router />
-      <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
