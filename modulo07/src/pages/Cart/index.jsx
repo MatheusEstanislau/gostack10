4b583d0ef737245c43615f9117482d3fe1d9e94e
MatheusEstanislau <matheus.estanislau7@gmail.com>
2020-11-26 16:16:57 -0300

    adicionada lógica com immer ao adicionar produtos no carrinho

--- modulo07/src/pages/Cart/index.jsx
+++ modulo07/src/pages/Cart/index.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable no-console */
+/* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 
@@ -7,9 +10,11 @@ import {
   MdDelete,
 } from 'react-icons/md';
 
+import { connect } from 'react-redux';
+
 import { Container, ProductTable, Total } from './styles';
 
-const Cart = () => {
+const Cart = ({ cart }) => {
   return (
     <Container>
       <ProductTable>
@@ -23,37 +28,36 @@ const Cart = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <img
-                src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_detalhe1.jpg?ts=1571078789?ims=280x280"
-                alt="Tênis"
-              />
-            </td>
-            <td>
-              <strong>Tênis muito massa</strong>
-              <span>R$129,90</span>
-            </td>
-            <td>
-              <div>
+          {cart.map(product => (
+            <tr key={product.id}>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormated}</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+                  <input type="number" readOnly value={product.amount} />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$258,80</strong>
+              </td>
+              <td>
                 <button type="button">
-                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  <MdDelete size={20} color="#7159c1" />
                 </button>
-                <input type="number" readOnly value={2} />
-                <button type="button">
-                  <MdAddCircleOutline size={20} color="#7159c1" />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$258,80</strong>
-            </td>
-            <td>
-              <button type="button">
-                <MdDelete size={20} color="#7159c1" />
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </ProductTable>
       <footer>
@@ -67,4 +71,8 @@ const Cart = () => {
   );
 };
 
-export default Cart;
+const mapStateToProps = state => ({
+  cart: state.cart,
+});
+
+export default connect(mapStateToProps)(Cart);
--- modulo07/src/store/modules/cart/reducer.js
+++ modulo07/src/store/modules/cart/reducer.js
@@ -1,8 +1,16 @@
-/* eslint-disable no-unused-vars */
+import produce from 'immer';
+
 const cart = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, action.product];
+      return produce(state, draft => {
+        const ProductIndex = draft.findIndex(p => p.id === action.product.id);
+        if (ProductIndex >= 0) {
+          draft[ProductIndex].amount += 1;
+        } else {
+          draft.push({ ...action.product, amount: 1 });
+        }
+      });
     default:
       return state;
   }
